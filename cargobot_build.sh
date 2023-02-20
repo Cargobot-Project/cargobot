73f0a720ade1ba2424351ba717abc006f0438dd0
Yağız Yaşar <yagizyasarr9@gmail.com>
2023-02-20 13:28:59 +0300

    Added dependency for the current directory script

--- cargobot_build.sh
+++ cargobot_build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+curdir=$(pwd -P)
+
+# Remove any pre-existing container named same with new container
+docker rm cargobot_container
 
 docker run --name cargobot_container \
 	--privileged \
-	-v ~/CS491/cargobot:/usr/cargobot \
+	-v $curdir:/usr/cargobot \
 	-it cargobot_image:latest bash
 

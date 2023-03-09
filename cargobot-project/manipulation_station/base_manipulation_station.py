4fdea9d5c84f27180e8354a5280855f47180decd
Yağız Yaşar <yagizyasarr9@gmail.com>
2023-03-09 21:46:45 +0300

    Manipulation station can be compiled.

--- cargobot-project/manipulation_station/base_manipulation_station.py
+++ cargobot-project/manipulation_station/base_manipulation_station.py
@@ -4,7 +4,7 @@ from pydrake.all import ( AddMultibodyPlantSceneGraph,
                          DiagramBuilder,Parser)
 
 class BaseManipulationStation:
-    def __init__(self,time_step: float,add_iiwa: bool = True,collision_model: str = "with_box_collision"):
+    def __init__(self, time_step: float, add_iiwa: bool = True, collision_model: str = "with_box_collision"):
         self.builder = DiagramBuilder()
         self.plant, self.scene_graph = AddMultibodyPlantSceneGraph(
             self.builder, time_step=time_step
@@ -12,7 +12,7 @@ class BaseManipulationStation:
         self.parser = Parser(self.plant)
         AddPackagePaths(self.parser)
         #Relative path, dont change the model yaml folder !
-        self.base_models = self.parser.AddAllModelsFromFile("../perception/models/all.dmd.yaml")
+        self.base_models = self.parser.AddAllModelsFromFile("/usr/cargobot/cargobot-project/perception/models/all.dmd.yaml")
         if add_iiwa:
             self.iiwa = AddIiwa(self.plant, collision_model=collision_model)
             self.wsg = AddWsg(self.plant, self.iiwa, roll=0.0, welded=True, sphere=False)
@@ -23,4 +23,27 @@ class BaseManipulationStation:
         #self.parser = Parser(self.plant)
         #self.query_output_port = self.scene_graph.GetOutputPort("query")
 
+    # get functions
+    def get_plant(self):
+        return self.plant
+    
+    def get_builder(self):
+        return self.builder
+    
+    def get_diagram(self):
+        return self.diagram
+    
+    def get_scene_graph(self):
+        return self.scene_graph
+    
+    def get_parser(self):
+        return self.parser
+    
+    def get_iiwa(self):
+        return self.iiwa
+    
+    def get_wsg(self):
+        return self.wsg
+
+    
 
--- cargobot-project/manipulation_station/manipulation_station.py
+++ cargobot-project/manipulation_station/manipulation_station.py
@@ -1,7 +1,7 @@
 from base_manipulation_station import BaseManipulationStation
 import numpy as np
 import os
-
+import time
 from pydrake.common import FindResourceOrThrow, temp_directory
 from pydrake.math import RigidTransform
 from pydrake.all import RotationMatrix
@@ -13,23 +13,24 @@ from pydrake.geometry import (
 )
 
 class ManipulationStation(BaseManipulationStation):
-    def __init__(self,time_step: float,add_iiwa: bool = True,collision_model: str = "with_box_collision"):
-        BaseManipulationStation.__init__(self,time_step,add_iiwa,collision_model)
+    def __init__(self, time_step: float, add_iiwa: bool = True, collision_model: str = "with_box_collision"):
+        super(ManipulationStation, self).__init__(time_step, add_iiwa, collision_model)
         self.meshcat = StartMeshcat()
         self.meshcat.Delete()
         self.meshcat.DeleteAddedControls()
         self.build_scenario()
-        super().plant.Finalize()
-        self.visualizer = MeshcatVisualizer.AddToBuilder(super().builder, super().scene_graph, self.meshcat,
+        super().get_plant().Finalize()
+        self.visualizer = MeshcatVisualizer.AddToBuilder(super().get_builder(), super().get_scene_graph(), self.meshcat,
         MeshcatVisualizerParams(role=Role.kIllustration))
         self.collision_visualizer = MeshcatVisualizer.AddToBuilder(
-        super().builder, super().scene_graph, self.meshcat,
-        MeshcatVisualizerParams(prefix="collision", role=Role.kProximity))
+                                    super().get_builder(), super().get_scene_graph(), self.meshcat,
+                                    MeshcatVisualizerParams(prefix="collision", role=Role.kProximity)
+                                )
         self.meshcat.SetProperty("collision", "visible", False)
 
-        self.diagram = super().builder.Build()
-        self.context = super().diagram.CreateDefaultContext()
-        self.plant_context = super().plant.GetMyContextFromRoot(self.context)
+        self.diagram = super().get_builder().Build()
+        self.context = super().get_diagram().CreateDefaultContext()
+        self.plant_context = super().get_plant().GetMyContextFromRoot(self.context)
 
     
     def build_scenario(self):
@@ -37,9 +38,13 @@ class ManipulationStation(BaseManipulationStation):
         final = [-0.5, -0.2, 0.0]
         X_O = {"initial": RigidTransform(RotationMatrix(), [0.6, 0.1, .1]),
                 "goal": RigidTransform(RotationMatrix(), final)}
-        box = super().plant.GetBodyByName("box-horizontal")
-        super().plant.SetDefaultFreeBodyPose(box, X_O["initial"])
+        box = super().get_plant().GetBodyByName("base_link_box-horizontal")
+        super().get_plant().SetDefaultFreeBodyPose(box, X_O["initial"])
+        
 ManipulationStation(1.0)
+
+while True:
+    time.sleep(1)
     
 
 

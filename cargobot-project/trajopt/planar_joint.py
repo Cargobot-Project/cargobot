325ba225e4d463e74b35498c65f57fe0a8f8f916
BekeUcar <berke.ucar@ug.bilkent.edu.tr>
2023-03-10 18:26:18 +0300

    3D Planar Joint Added

--- cargobot-project/trajopt/planar_joint.py
+++ cargobot-project/trajopt/planar_joint.py
@@ -32,7 +32,7 @@ from pydrake.all import (AddMultibodyPlantSceneGraph, Box,
                          FixedOffsetFrame, JointIndex, Parser, PlanarJoint,
                          RandomGenerator, RigidTransform, RollPitchYaw,
                          RotationMatrix, Simulator,
-                         UniformlyRandomRotationMatrix, PlanarJoint, Context,JointActuator, PrismaticJoint)
+                         UniformlyRandomRotationMatrix, PlanarJoint, Context,JointActuator, PrismaticJoint, RevoluteJoint)
 
 from manipulation import running_as_notebook
 from manipulation.scenarios import (AddRgbdSensor, AddShape, ycb)
@@ -48,30 +48,37 @@ builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0005)
 
 parser = Parser(plant)
-instances = parser.AddModels("cargobot-models/scene_without_robot.dmd.yaml")
+instances = parser.AddModelFromFile("cargobot-models/table_top_with_box.sdf")
 
 false_body = plant.AddRigidBody(
-        "false_body", instances[1],
+        "false_body", instances,
         SpatialInertia(0, [0, 0, 0], UnitInertia(0, 0, 0)))
-mobile_base_y = plant.AddJoint(PrismaticJoint(
-        "mobile_base_y", plant.GetFrameByName("table_top_link"), plant.GetFrameByName("false_body"), 
-        [0, 1, 0], -3, 3))
-
 false_body2 = plant.AddRigidBody(
-        "false_body2", instances[1],
+        "false_body2", instances,
         SpatialInertia(0, [0, 0, 0], UnitInertia(0, 0, 0)))
-mobile_base_x = plant.AddJoint(PrismaticJoint(
-        "mobile_base_x", plant.GetFrameByName("table_top_link"), plant.GetFrameByName("false_body2"), 
-        [1, 0, 0], -3, 3))
 
+mobile_base_y = plant.AddJoint(PrismaticJoint(
+        "mobile_base_y", plant.GetFrameByName("world"), plant.GetFrameByName("false_body"), 
+        [0, 1, 0], -3, 3))
 plant.AddJointActuator("mobile_base_y_actuator", mobile_base_y)
+
+mobile_base_x = plant.AddJoint(PrismaticJoint(
+        "mobile_base_x", plant.GetFrameByName("false_body"), plant.GetFrameByName("false_body2"), 
+        [0, 0, 1], -3, 3))
+mobile_base_x.set_default_translation(0.3)
 plant.AddJointActuator("mobile_base_x_actuator", mobile_base_x)
 
+mobile_base_theta = plant.AddJoint(RevoluteJoint(
+        "mobile_base_theta", plant.GetFrameByName("false_body2"), plant.GetFrameByName("box"), 
+        [0, -1, 0],  -np.pi, np.pi))
+plant.AddJointActuator("mobile_base_theta_actuator", mobile_base_theta)
+
 plant.Finalize()
+
 visualizer = MeshcatVisualizer.AddToBuilder(
     builder, scene_graph, meshcat)
 
-box = plant.GetModelInstanceByName("table_top")
+
 plant_context = plant.CreateDefaultContext()
 
 kp=[1] * plant.num_positions()
@@ -83,7 +90,7 @@ print(plant.num_velocities())
 planar_controller = builder.AddSystem(
     InverseDynamicsController(plant, kp, ki, kd, False))
 planar_controller.set_name("planar_controller")
-builder.Connect(plant.get_state_output_port(box),
+builder.Connect(plant.get_state_output_port(instances),
                 planar_controller.get_input_port_estimated_state())
 builder.Connect(planar_controller.get_output_port_control(),
                 plant.get_actuation_input_port())
@@ -92,7 +99,7 @@ diagram = builder.Build()
 context = diagram.CreateDefaultContext()
 
 planar_controller.GetInputPort('desired_state').FixValue(
-    planar_controller.GetMyMutableContextFromRoot(context), [1,0,1,0])
+    planar_controller.GetMyMutableContextFromRoot(context), [1,0,1, 0, 0, 0])
 
 simulator = Simulator(diagram, context)
 visualizer.StartRecording()

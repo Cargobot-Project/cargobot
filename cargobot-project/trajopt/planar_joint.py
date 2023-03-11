f7667c105bf494cacb31505ec3e7760db6282287
BekeUcar <berke.ucar@ug.bilkent.edu.tr>
2023-03-12 00:20:27 +0300

    IIWA mobile base added

--- cargobot-project/trajopt/planar_joint.py
+++ cargobot-project/trajopt/planar_joint.py
@@ -9,6 +9,7 @@ from pydrake.geometry import (
     Role,
     StartMeshcat,
 )
+
 from manipulation.utils import AddPackagePaths, FindResource
 from manipulation.meshcat_utils import PublishPositionTrajectory
 from IPython.display import clear_output
@@ -32,7 +33,7 @@ from pydrake.all import (AddMultibodyPlantSceneGraph, Box,
                          FixedOffsetFrame, JointIndex, Parser, PlanarJoint,
                          RandomGenerator, RigidTransform, RollPitchYaw,
                          RotationMatrix, Simulator,
-                         UniformlyRandomRotationMatrix, PlanarJoint, Context,JointActuator, PrismaticJoint, RevoluteJoint)
+                         UniformlyRandomRotationMatrix, PlanarJoint, Context,JointActuator, PrismaticJoint, RevoluteJoint, WeldJoint)
 
 from manipulation import running_as_notebook
 from manipulation.scenarios import (AddRgbdSensor, AddShape, ycb)
@@ -45,52 +46,58 @@ meshcat = StartMeshcat()
 
 meshcat.Flush()
 builder = DiagramBuilder()
-plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0005)
+plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.05)
 
 parser = Parser(plant)
-instances = parser.AddModelFromFile("cargobot-models/table_top_with_box.sdf")
 
-false_body = plant.AddRigidBody(
-        "false_body", instances,
-        SpatialInertia(0, [0, 0, 0], UnitInertia(0, 0, 0)))
+scene_models = parser.AddModels("cargobot-models/scene_without_robot.dmd.yaml")
+#iiwa = plant.GetModelInstanceByName("box")
+
+"""false_body = plant.AddRigidBody(
+        "false_body", iiwa,
+        SpatialInertia(1, [0, 0, 0], UnitInertia(0.0041666666666666675, 0.0041666666666666675, 0.001666666666666667)))
 false_body2 = plant.AddRigidBody(
-        "false_body2", instances,
-        SpatialInertia(0, [0, 0, 0], UnitInertia(0, 0, 0)))
+        "false_body2", iiwa,
+        SpatialInertia(1, [0, 0, 0], UnitInertia(0.0041666666666666675, 0.0041666666666666675, 0.001666666666666667)))
 
 mobile_base_y = plant.AddJoint(PrismaticJoint(
-        "mobile_base_y", plant.GetFrameByName("world"), plant.GetFrameByName("false_body"), 
-        [0, 1, 0], -3, 3))
+        "mobile_base_y", plant.GetFrameByName("table_top_link"), plant.GetFrameByName("false_body"), 
+        [0, 1, 0], -.3, .3))
 plant.AddJointActuator("mobile_base_y_actuator", mobile_base_y)
 
 mobile_base_x = plant.AddJoint(PrismaticJoint(
         "mobile_base_x", plant.GetFrameByName("false_body"), plant.GetFrameByName("false_body2"), 
-        [0, 0, 1], -3, 3))
-mobile_base_x.set_default_translation(0.3)
+        [1, 0, 0], -.3, .3))
 plant.AddJointActuator("mobile_base_x_actuator", mobile_base_x)
 
 mobile_base_theta = plant.AddJoint(RevoluteJoint(
         "mobile_base_theta", plant.GetFrameByName("false_body2"), plant.GetFrameByName("box"), 
-        [0, -1, 0],  -np.pi, np.pi))
+        [0, 0, 1],  -np.pi, np.pi))
 plant.AddJointActuator("mobile_base_theta_actuator", mobile_base_theta)
+
+#weld_joint = plant.AddJoint(WeldJoint("weld_joint", plant.GetFrameByName("box"), plant.GetFrameByName("iiwa_link_0"), RigidTransform()))
+plant.WeldFrames(plant.GetFrameByName("box"), plant.GetFrameByName("iiwa_link_0"))
+
+mobile_base_theta.set_acceleration_limits([0], [1])
+mobile_base_y.set_acceleration_limits([0], [1])
+mobile_base_x.set_acceleration_limits([0], [1])
+"""
 
 plant.Finalize()
 
 visualizer = MeshcatVisualizer.AddToBuilder(
     builder, scene_graph, meshcat)
 
-
 plant_context = plant.CreateDefaultContext()
 
 kp=[1] * plant.num_positions()
 ki=[1] * plant.num_positions()
 kd=[1] * plant.num_positions()
-print(plant.num_positions())
-print(plant.num_velocities())
 
 planar_controller = builder.AddSystem(
     InverseDynamicsController(plant, kp, ki, kd, False))
 planar_controller.set_name("planar_controller")
-builder.Connect(plant.get_state_output_port(instances),
+builder.Connect(plant.get_state_output_port(),
                 planar_controller.get_input_port_estimated_state())
 builder.Connect(planar_controller.get_output_port_control(),
                 plant.get_actuation_input_port())
@@ -98,14 +105,19 @@ builder.Connect(planar_controller.get_output_port_control(),
 diagram = builder.Build()
 context = diagram.CreateDefaultContext()
 
+states = [1, 1, 0, -1.57, 0.1, 0, -1.2, 0, 1.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 planar_controller.GetInputPort('desired_state').FixValue(
-    planar_controller.GetMyMutableContextFromRoot(context), [1,0,1, 0, 0, 0])
+    planar_controller.GetMyMutableContextFromRoot(context), states)
 
 simulator = Simulator(diagram, context)
+"""simulator.get_mutable_integrator().set_fixed_step_mode(True)"""
 visualizer.StartRecording()
-simulator.AdvanceTo(15.0)
+"""SVG(pydot.graph_from_dot_data(diagram.GetGraphvizString())[0]).create_svg()"""
 
+print(context)
+simulator.AdvanceTo(12.0)
 visualizer.StopRecording()
 visualizer.PublishRecording()
+
 while True:
     continue
